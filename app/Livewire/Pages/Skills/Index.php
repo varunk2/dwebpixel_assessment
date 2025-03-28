c00e7c06a46a3e424295293587aa84ad9251c1d5
varun_astra <varun.sharma@getastra.com>
2025-03-28 17:28:22 +0530

    feat: Added all the functions required for Add and Edit functionality in Livewire Component

--- app/Livewire/Pages/Skills/Index.php
+++ app/Livewire/Pages/Skills/Index.php
@@ -2,11 +2,70 @@
 
 namespace App\Livewire\Pages\Skills;
 
+use App\Models\Skills;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public array $skills = [];
+    public $skills;
+
+    public $name = "";
+
+    public $selectedRows = [];
+    public $allSelected = false;
+    public $editFlag = false;
+    public $currentEditing;
+
+    public function mount(Skills $skills) {
+        $this->skills = $skills->all();
+
+        $this->fill(
+            $skills->only('id', 'name')
+        );
+    }
+
+    public function updateAllSelected($value) {
+        $this->selectedRows = $value ? $this->skills->pluck('id')->toArray() : [];
+    }
+
+    public function toggleEditFlag($id) {
+        $this->editFlag = true;
+        $this->currentEditing = $id;
+        $this->name = $this->skills->where('id', $id)->first()->name;
+    }
+
+    public function deletedSelectedRows() {
+        $rowsCount = count($this->selectedRows);
+        Skills::whereIn('id', $this->selectedRows)->delete();
+        $this->selectedRows = [];
+        session()->flash('success', "$rowsCount rows deleted successfully");
+        return $this->redirect('/admin/skills');
+    }
+
+    public function save() {
+        Skills::create($this->only('name'));
+
+        session()->flash('success', 'New skill added successfully');
+
+        return $this->redirect('/admin/skills');
+    }
+
+    public function update() {
+        Skills::where('id', $this->currentEditing)
+                ->update(['name' => $this->name]);
+
+        session()->flash('success', 'Skill successfully updated');
+
+        return $this->redirect('/admin/skills');
+    }
+
+    public function delete($id) {
+        $skill = Skills::destroy($id);
+        session()->flash('success', 'Skill deleted successfully');
+
+        return $this->redirect('/admin/skills');
+    }
+
 
     public function render()
     {
