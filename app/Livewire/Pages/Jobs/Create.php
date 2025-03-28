732938332fb1c95f06737c175d45493f54240332
varun_astra <varun.sharma@getastra.com>
2025-03-28 18:24:14 +0530

    feat: Added properties required for creating a job post.

--- app/Livewire/Pages/Jobs/Create.php
+++ app/Livewire/Pages/Jobs/Create.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire\Pages\Jobs;
 
+use App\Models\Skills;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\Attributes\Validate;
 
 class Create extends Component
 {
+    use WithFileUploads;
+
+    #[Validate('image|max:1024')]
+    public $photo;
+
+    public $skills;
+    public $title = '';
+    public $description = '';
+    public $experience = '';
+    public $salary = '';
+
+    public function mount()
+    {
+        $this->skills = Skills::all();
+    }
     public function render()
     {
         return view('livewire.pages.jobs.create');
