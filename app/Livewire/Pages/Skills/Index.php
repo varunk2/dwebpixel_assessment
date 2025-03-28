e4f9505094bc0dcc92ac9be0a71234b2bbc03368
varun_astra <varun.sharma@getastra.com>
2025-03-28 15:13:33 +0530

    add: Added skills array.

--- app/Livewire/Pages/Skills/Index.php
+++ app/Livewire/Pages/Skills/Index.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public array $skills = [];
+
     public function render()
     {
         return view('livewire.pages.skills.index');
