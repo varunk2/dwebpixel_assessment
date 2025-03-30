e2f78db4074a5bb78ef840976ae486b6c0be2d90
varun_astra <varun.sharma@getastra.com>
2025-03-30 19:07:13 +0530

    feat: Added dynamic jobs access

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobPosts;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -9,48 +10,7 @@ use Inertia\Response;
 class DashboardController extends Controller
 {
     public function index(): Response {
-        $jobs = [
-            [
-                "id" => 1,
-                "title" => "Sr. Full Stack Developer",
-                "description" => "You will be responsible for designing, developing, and maintaining robust and scalable web applications from end to end. You must have a deep understanding of both frontend and backend development, thrives in a collaborative environment, and is passionate about delivering high-quality software solutions",
-                "company_name" => "DWebPixel Technologies Pvt. Ltd.",
-                "company_logo" => asset('logo-3.svg'),
-                "experience" => "4-5 Yrs",
-                "salary" => "4.5-8 Lacs PA",
-                "location" => "Remote",
-                "skills" => [
-                    "Laravel",
-                    "React",
-                    "Vue",
-                    "MySQL",
-                ],
-                "extra" => [
-                    "Remote",
-                    "Full-Time",
-                ]
-            ],
-            [
-                "id" => 2,
-                "title" => "Sr. Frontend Developer",
-                "description" => "You will leverage your expertise in modern frontend technologies and best practices to create exceptional user experiences.",
-                "company_name" => "Devsbuddy Technologies Pvt. Ltd.",
-                "company_logo" => asset('logo-2.svg'),
-                "experience" => "3-4 Yrs",
-                "salary" => "2.5-4 Lacs PA",
-                "location" => "Remote",
-                "skills" => [
-                    "React",
-                    "Vue",
-                ],
-                "extra" => [
-                    "Remote",
-                    "Full-Time",
-                ]
-            ]
-        ];
-
-        // $jobs = json_encode($jobs);
+        $jobs = JobPosts::all();
 
         return Inertia::render('Dashboard', compact('jobs'));
     }
